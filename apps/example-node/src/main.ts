0795c2cb3e96c2c5dc999f68f09e4248733d4ea7
Kostas Makris <kostasmakris23@gmail.com>
2025-07-27 12:36:00 +0300

    chore: clear example app

--- apps/example-node/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { StoreSdk } from '@store-sdk/core';
-import { ProductRequest } from 'packages/core/src/lib/types/store/product/product.request';
-
-StoreSdk.init({ baseUrl: '' });
-StoreSdk.products().list({} as ProductRequest);
