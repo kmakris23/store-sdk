2221b260fa82050550a2bf2f26c47fb825e93477
Kostas Makris <kostasmakris23@gmail.com>
2025-07-28 00:11:12 +0300

    chore: add `example-angular` app

---
+++ apps/example-angular/src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcome } from './nx-welcome';
+import { StoreSdk } from '@store-sdk/core';
+
+@Component({
+  imports: [NxWelcome, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App implements OnInit {
+  protected title = 'example-angular';
+
+  async ngOnInit() {
+    StoreSdk.init({ baseUrl: 'https://herbally.gr' });
+    const products = await StoreSdk.products().list();
+    console.log(products);
+  }
+}
