3c60ab2d131def484d4579e756a9db25e5824175
Kostas Makris <kostasmakris23@gmail.com>
2025-07-27 10:58:08 +0300

    fix: update `tsup` config

--- apps/example-node/src/main.ts
+++ apps/example-node/src/main.ts
@@ -1,4 +1,5 @@
 import { StoreSdk } from '@store-sdk/core';
+import { ProductRequest } from 'packages/core/src/lib/types/store/product/product.request';
 
 StoreSdk.init({ baseUrl: '' });
-StoreSdk.products().list();
+StoreSdk.products().list({} as ProductRequest);
--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -4,8 +4,9 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  splitting: false,
   sourcemap: true,
   clean: true,
-  outDir: 'dist',
+  target: 'es2020',
+  treeshake: true,
+  external: ['axios', 'qs', 'date-fns']
 });
