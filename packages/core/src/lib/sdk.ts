2978784e7f4305ac7d915967dffbd55e0cdc08a0
Kostas Makris <kostasmakris23@gmail.com>
2025-08-12 11:49:31 +0300

    feat(core)!: move store api services under `store` property
    BREAKING CHANGE: Store API services have been moved under `store` property in `StoreSdk`.
    For example, use `StoreSdk.store.cart.get()` instead of  `StoreSdk.cart.get()`

--- packages/core/src/lib/sdk.ts
+++ packages/core/src/lib/sdk.ts
@@ -1,17 +1,4 @@
 import { StoreSdkConfig } from './configs/sdk.config.js';
-import { CartCouponService } from './services/store/cart.coupon.service.js';
-import { CartItemService } from './services/store/cart.item.service.js';
-import { CartService } from './services/store/cart.service.js';
-import { CheckoutService } from './services/store/checkout.service.js';
-import { OrderService } from './services/store/order.service.js';
-import { ProductAttributeService } from './services/store/product.attribute.service.js';
-import { ProductAttributeTermService } from './services/store/product.attribute.term.service.js';
-import { ProductBrandService } from './services/store/product.brand.service.js';
-import { ProductCategoryService } from './services/store/product.category.service.js';
-import { ProductCollectionDataService } from './services/store/product.collection.data.service.js';
-import { ProductReviewService } from './services/store/product.review.service.js';
-import { ProductService } from './services/store/product.service.js';
-import { ProductTagService } from './services/store/product.tag.service.js';
 import { StoreSdkState } from './types/sdk.state.js';
 import { StoreSdkEventEmitter } from './sdk.event.emitter.js';
 import { createHttpClient } from './services/api.js';
@@ -19,24 +6,12 @@ import { addCartTokenInterceptors } from './interceptors/cart.token.interceptor.
 import { addNonceInterceptors } from './interceptors/nonce.interceptor.js';
 import { addCartLoadingInterceptors } from './interceptors/cart.loading.interceptor.js';
 import { addSimpleJwtLoginInterceptors } from './interceptors/simple.jwt.login.interceptors.js';
+import { StoreService } from './services/store.service.js';
 
 export class Sdk {
-  private _tags!: ProductTagService;
-  private _orders!: OrderService;
-  private _brands!: ProductBrandService;
-  private _checkout!: CheckoutService;
-  private _products!: ProductService;
-  private _reviews!: ProductReviewService;
-  private _categories!: ProductCategoryService;
-  private _attributes!: ProductAttributeService;
-  private _attributesTerms!: ProductAttributeTermService;
-  private _collectionData!: ProductCollectionDataService;
-
-  private _cart!: CartService;
-  private _cartItems!: CartItemService;
-  private _cartCoupons!: CartCouponService;
-
   state: StoreSdkState = {};
+
+  private _store!: StoreService;
 
   private _initialized = false;
 
@@ -45,36 +20,7 @@ export class Sdk {
   public async init(config: StoreSdkConfig): Promise<void> {
     if (this._initialized) return;
 
-    this._tags = new ProductTagService(this.state, config, this.events);
-    this._orders = new OrderService(this.state, config, this.events);
-    this._brands = new ProductBrandService(this.state, config, this.events);
-    this._checkout = new CheckoutService(this.state, config, this.events);
-    this._reviews = new ProductReviewService(this.state, config, this.events);
-    this._products = new ProductService(this.state, config, this.events);
-    this._categories = new ProductCategoryService(
-      this.state,
-      config,
-      this.events
-    );
-    this._attributes = new ProductAttributeService(
-      this.state,
-      config,
-      this.events
-    );
-    this._attributesTerms = new ProductAttributeTermService(
-      this.state,
-      config,
-      this.events
-    );
-    this._collectionData = new ProductCollectionDataService(
-      this.state,
-      config,
-      this.events
-    );
-
-    this._cart = new CartService(this.state, config, this.events);
-    this._cartItems = new CartItemService(this.state, config, this.events);
-    this._cartCoupons = new CartCouponService(this.state, config, this.events);
+    this._store = new StoreService(this.state, config, this.events);
 
     createHttpClient({
       baseURL: config.baseUrl,
@@ -87,7 +33,7 @@ export class Sdk {
     const allPlugins = [...(config.plugins ?? [])];
     for (const plugin of allPlugins) {
       if (plugin.id === 'simple-jwt-login') {
-        addSimpleJwtLoginInterceptors(config, plugin.getConfig());
+        addSimpleJwtLoginInterceptors(config, plugin.getConfig(), this._store);
       }
 
       plugin.init();
@@ -98,111 +44,15 @@ export class Sdk {
 
     this._initialized = true;
 
-    await this._cart.get();
+    await this._store.cart.get();
   }
 
   /**
-   * Product Tags API
+   * Store API
    */
-  get tags() {
+  get store() {
     this.throwIfNotInitized();
-    return this._tags;
-  }
-
-  /**
-   * Order API
-   */
-  get orders() {
-    this.throwIfNotInitized();
-    return this._orders;
-  }
-
-  /**
-   * Product Brands API
-   */
-  get brands() {
-    this.throwIfNotInitized();
-    return this._brands;
-  }
-
-  /**
-   * Checkout API
-   */
-  get checkout() {
-    this.throwIfNotInitized();
-    return this._checkout;
-  }
-
-  /**
-   * Product Reviews API
-   */
-  get reviews() {
-    this.throwIfNotInitized();
-    return this._reviews;
-  }
-
-  /**
-   * Products API
-   */
-  get products() {
-    this.throwIfNotInitized();
-    return this._products;
-  }
-
-  /**
-   * Product Categories API
-   */
-  get categories() {
-    this.throwIfNotInitized();
-    return this._categories;
-  }
-
-  /**
-   * Product Attributes API
-   */
-  get attributes() {
-    this.throwIfNotInitized();
-    return this._attributes;
-  }
-
-  /**
-   * Product Attribute Terms API
-   */
-  get attributesTerms() {
-    this.throwIfNotInitized();
-    return this._attributesTerms;
-  }
-
-  /**
-   * Product Collection Data API
-   */
-  get collectionData() {
-    this.throwIfNotInitized();
-    return this._collectionData;
-  }
-
-  /**
-   * Cart API
-   */
-  get cart() {
-    this.throwIfNotInitized();
-    return this._cart;
-  }
-
-  /**
-   * Cart Items API
-   */
-  get cartItems() {
-    this.throwIfNotInitized();
-    return this._cartItems;
-  }
-
-  /**
-   * Cart Coupons API
-   */
-  get cartCoupons() {
-    this.throwIfNotInitized();
-    return this._cartCoupons;
+    return this._store;
   }
 
   private throwIfNotInitized() {
---
+++ packages/core/src/lib/services/store.service.ts
@@ -0,0 +1,153 @@
+import { StoreSdkConfig } from '../configs/sdk.config.js';
+import { StoreSdkEventEmitter } from '../sdk.event.emitter.js';
+import { StoreSdkState } from '../types/sdk.state.js';
+import { CartCouponService } from './store/cart.coupon.service.js';
+import { CartItemService } from './store/cart.item.service.js';
+import { CartService } from './store/cart.service.js';
+import { CheckoutService } from './store/checkout.service.js';
+import { OrderService } from './store/order.service.js';
+import { ProductAttributeService } from './store/product.attribute.service.js';
+import { ProductAttributeTermService } from './store/product.attribute.term.service.js';
+import { ProductBrandService } from './store/product.brand.service.js';
+import { ProductCategoryService } from './store/product.category.service.js';
+import { ProductCollectionDataService } from './store/product.collection.data.service.js';
+import { ProductReviewService } from './store/product.review.service.js';
+import { ProductService } from './store/product.service.js';
+import { ProductTagService } from './store/product.tag.service.js';
+
+export class StoreService {
+  private _tags!: ProductTagService;
+  private _orders!: OrderService;
+  private _brands!: ProductBrandService;
+  private _checkout!: CheckoutService;
+  private _products!: ProductService;
+  private _reviews!: ProductReviewService;
+  private _categories!: ProductCategoryService;
+  private _attributes!: ProductAttributeService;
+  private _attributesTerms!: ProductAttributeTermService;
+  private _collectionData!: ProductCollectionDataService;
+
+  private _cart!: CartService;
+  private _cartItems!: CartItemService;
+  private _cartCoupons!: CartCouponService;
+
+  constructor(
+    state: StoreSdkState,
+    config: StoreSdkConfig,
+    events: StoreSdkEventEmitter
+  ) {
+    this._tags = new ProductTagService(state, config, events);
+    this._orders = new OrderService(state, config, events);
+    this._brands = new ProductBrandService(state, config, events);
+    this._checkout = new CheckoutService(state, config, events);
+    this._reviews = new ProductReviewService(state, config, events);
+    this._products = new ProductService(state, config, events);
+    this._categories = new ProductCategoryService(state, config, events);
+    this._attributes = new ProductAttributeService(state, config, events);
+    this._attributesTerms = new ProductAttributeTermService(
+      state,
+      config,
+      events
+    );
+    this._collectionData = new ProductCollectionDataService(
+      state,
+      config,
+      events
+    );
+
+    this._cart = new CartService(state, config, events);
+    this._cartItems = new CartItemService(state, config, events);
+    this._cartCoupons = new CartCouponService(state, config, events);
+  }
+
+  /**
+   * Product Tags API
+   */
+  get tags() {
+    return this._tags;
+  }
+
+  /**
+   * Order API
+   */
+  get orders() {
+    return this._orders;
+  }
+
+  /**
+   * Product Brands API
+   */
+  get brands() {
+    return this._brands;
+  }
+
+  /**
+   * Checkout API
+   */
+  get checkout() {
+    return this._checkout;
+  }
+
+  /**
+   * Product Reviews API
+   */
+  get reviews() {
+    return this._reviews;
+  }
+
+  /**
+   * Products API
+   */
+  get products() {
+    return this._products;
+  }
+
+  /**
+   * Product Categories API
+   */
+  get categories() {
+    return this._categories;
+  }
+
+  /**
+   * Product Attributes API
+   */
+  get attributes() {
+    return this._attributes;
+  }
+
+  /**
+   * Product Attribute Terms API
+   */
+  get attributesTerms() {
+    return this._attributesTerms;
+  }
+
+  /**
+   * Product Collection Data API
+   */
+  get collectionData() {
+    return this._collectionData;
+  }
+
+  /**
+   * Cart API
+   */
+  get cart() {
+    return this._cart;
+  }
+
+  /**
+   * Cart Items API
+   */
+  get cartItems() {
+    return this._cartItems;
+  }
+
+  /**
+   * Cart Coupons API
+   */
+  get cartCoupons() {
+    return this._cartCoupons;
+  }
+}
